27c6d302b655b68c07fe792afb9741744ed10fc2
Manu Cutillas <manucutillas@outlook.com>
2018-05-25 15:09:38 +0200

    Added rethinkdb

---
+++ modules/db/rethink.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+"fmt"
+"log"
+
+r "gopkg.in/gorethink/gorethink.v4"
+)
+// RethinkDB
+func connect() {
+	url := 'd'
+	session, err := r.Connect(r.ConnectOpts{
+		Address: url,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	res, err := r.Expr("Hello World").Run(session)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var response string
+	err = res.One(&response)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(response)
+
+	// Output:
+	// Hello World
+}
